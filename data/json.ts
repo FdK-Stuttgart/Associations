f50b9d11ea7ce6b4f8ff9ae3369978c7fbb42b7e
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-05-25 18:43:11 +0200

    [data] wip - store information in the json

--- data/json.ts
+++ data/json.ts
@@ -13,9 +13,9 @@ function processTableRowUmap(requestFormat, row) {
     const normAddr     = g.normalizeAddress(address)
     const descMarkdown = desc.split(/\s+/).map(g.encodeLine)
 
-    var logosMarkdown = ""
+    let logosMarkdown = ""
     if (logos) {
-        var arrMarkdown = logos.split(/\s+/).map((s) => {
+        let arrMarkdown = logos.split(/\s+/).map((s) => {
             return "{{" + s + "}}\n"
         })
         logosMarkdown = arrMarkdown.concat()
@@ -24,10 +24,10 @@ function processTableRowUmap(requestFormat, row) {
     else {
         console.log("logos " + logos + "; " + name)
     }
-    // var allFeatures
+    // let allFeatures
     if (coordinates) {
         // console.log('coordinates : '+ coordinates)
-        var obj: any =
+        let obj: any =
             [{
                 "type": "Feature",
                 "properties": {
@@ -51,7 +51,7 @@ function processTableRowUmap(requestFormat, row) {
                     "coordinates": coordinates.split(/\s+/)
                     // (read-string (format "[%s]" coordinates))
                 }}]
-        var allFeatures: JSON = <JSON> obj
+        let allFeatures: JSON = <JSON> obj
         return allFeatures
     }
     else {
@@ -61,6 +61,7 @@ function processTableRowUmap(requestFormat, row) {
 
 function processTableRowAngular(requestFormat, row) {
     const address      = row[o._address]
+    const contact      = row[o._contact]
     const cityDistrict = row[o._cityDistrict]
     const name         = row[o._name]
     const desc         = row[o._desc]
@@ -68,112 +69,175 @@ function processTableRowAngular(requestFormat, row) {
     const activity     = row[o._activity]
     const coordinates  = row[o._coordinates]
     const logos        = row[o._logo]
+    const links        = row[o._webPage]
     const normAddr     = g.normalizeAddress(address)
     const descMarkdown = desc.split(/\s+/).map(g.encodeLine)
 
-    var logosMarkdown = ""
+    let logosMarkdown = ""
     if (logos) {
-        var arrMarkdown = logos.split(/\s+/).map((s) => {
+        let arrMarkdown = logos.split(/\s+/).map((s) => {
             return "{{" + s + "}}\n"
         })
         logosMarkdown = arrMarkdown.concat()
         // console.log("arrMarkdown.concat(): " + arrMarkdown.concat())
     }
     else {
-        console.log("logos " + logos + "; " + name)
+        // console.log("logos " + logos + "; " + name)
     }
 
     // TODO send HTTP request with this json-obj to an API service. This service
     // inserts the data in the dbase
 
     if (coordinates) {
-        // console.log('coordinates : '+ coordinates)
-        var obj: any
-        obj = {
-            "addressLine1?": "",
-            "addressLine2?": "",
-            "addressLine3?": "",
-            "street?": "",
-            "postcode?": "",
-            "city?": "",
-            "country?": "",
+        let addrLines = []
+        if (normAddr != "keine öffentliche Anschrift") {
+            addrLines = normAddr.split(/, /)
         }
-        var Address : JSON = <JSON> obj
 
-        obj = {
-            "lat": 0,
-            "lng": 0,
+        // See association.ts
+        let obj : any
+        {
+            let postcode : String  // TODO is postcode a string or integer?
+            let city : String
+            let postcode_city = addrLines[1]
+            if (postcode_city) {
+                let postcode_city_split = postcode_city.split(/\s+/)
+                postcode = postcode_city_split[0]
+                city = postcode_city_split[1]
+            }
+            // TODO better parsing of the address-field is needed
+            // any undefined addressLine is pruned from the JSON object
+            obj = {
+                "addressLine1?": addrLines[0],
+                "addressLine2?": addrLines[1],
+                "addressLine3?": addrLines[2],
+                "street?": addrLines[0],
+                "postcode?": postcode,
+                "city?": city,
+                "country?": "",
+            }
         }
-        var LatLng : JSON = <JSON> obj
+        let Address : JSON = <JSON> obj
 
+        const lat_lon = coordinates.split(/\s+/).map(parseFloat)
         obj = {
-            "id?": "",
-            "name?": "",
-            "mail?": "",
-            "phone?": "",
-            "fax?": "",
-            "associationId?": "",
+            "lat": lat_lon[0],
+            "lng": lat_lon[1],
         }
-        var Contact : JSON = <JSON> obj
+        let LatLng : JSON = <JSON> obj
 
-        obj = {
-            "id?": "",
-            "linkText?": "",
-            "url": "",
-            "associationId?": "",
+        let arrContacts = new Array()
+        if (contact) {
+            // we'got just 1 contact
+            let Contact : JSON
+            // console.log("contact: " + contact)
+            // TODO better parsing of the contact-field is needed
+            let contactDetails = contact.split(/\n/)
+            obj = {
+                "id?": "",
+                "name?": "",
+                "mail?": contactDetails[1],
+                "phone?": contactDetails[0],
+                "fax?": "",
+                "associationId?": "",
+            }
+            Contact = <JSON> obj
+            arrContacts.push(Contact)
         }
-        var Link : JSON = <JSON> obj
 
-        // export enum SocialMediaPlatform {
-        //     FACEBOOK = 'Facebook',
-        //     INSTAGRAM = 'Instagram',
-        //     TWITTER = 'Twitter',
-        //     PINTEREST = 'Pinterest',
-        //     SNAPCHAT = 'Snapchat',
-        //     LINKEDIN = 'LinkedIn',
-        //     WHATSAPP = 'WhatsApp',
-        //     YOUTUBE = 'YouTube',
-        //     OTHER = 'Other'
-        // }
-        var SocialMediaPlatform = 'Facebook'
-
-        obj = {
-            "platform?": SocialMediaPlatform,
+        let arrLinks = new Array()
+        if (links) {
+            let linkList = links.split(/\s+/)
+            for (var i = 0; i < linkList.length; i++) {
+                var url = linkList[i]
+                obj = {
+                    "id?": "",
+                    "linkText?": "",
+                    "url": url,
+                    "associationId?": "",
+                }
+                let Link : JSON = <JSON> obj
+                arrLinks.push(Link)
+            }
         }
-        var SocialMediaLinkBase : JSON = <JSON> obj
 
-        // extends Link
-        var SocialMediaLink = { ...Link, ...SocialMediaLinkBase}
+        let arrSocialMedias = new Array()
+        {
+            // TODO for the social media classification see `encodeLine`
+            obj = {
+                "id?": "",
+                "linkText?": "",
+                "url": "",
+                "associationId?": "",
+            }
+            let Link : JSON = <JSON> obj
+            // export enum SocialMediaPlatform {
+            //     FACEBOOK = 'Facebook',
+            //     INSTAGRAM = 'Instagram',
+            //     TWITTER = 'Twitter',
+            //     PINTEREST = 'Pinterest',
+            //     SNAPCHAT = 'Snapchat',
+            //     LINKEDIN = 'LinkedIn',
+            //     WHATSAPP = 'WhatsApp',
+            //     YOUTUBE = 'YouTube',
+            //     OTHER = 'Other'
+            // }
+            let SocialMediaPlatform = 'Facebook'
+            obj = {
+                "platform?": SocialMediaPlatform,
+            }
+            let SocialMediaLinkBase : JSON = <JSON> obj
+            // extends Link
+            let SocialMediaLink = { ...Link, ...SocialMediaLinkBase}
+            arrSocialMedias.push(SocialMediaLink)
+        }
 
         obj = {
             "format?": 'plain', // 'plain' | 'html'
             "text": "",
         }
-        var TextBlock : JSON = <JSON> obj
+        let TextBlock : JSON = <JSON> obj
 
-        obj = {
-            "id?": "",
-            "url": "",
-            "altText?": "",
-            "associationId?": "",
+        let arrImages = new Array()
+        if (logos) {
+            let logoList = logos.split(/\s+/)
+            for (var i = 0; i < logoList.length; i++) {
+                var url = logoList[i]
+                obj = {
+                    "id?": "",
+                    "url": url,
+                    "altText?": "",
+                    "associationId?": "",
+                }
+                let Image : JSON = <JSON> obj
+                arrImages.push(Image)
+            }
         }
-        var Image : JSON = <JSON> obj
+
+        let activityList
+        if (activity) {
+            activityList = activity.split(/, /)
+        }
+        let districtList
+        if (cityDistrict) {
+            districtList = cityDistrict.split(/, /)
+        }
 
         obj = {
             "id": "",
-            "name": "",
-            "shortName?": "",
+            "name": name,
+            "shortName?": undefined,
             "goals?": TextBlock,
             "activities?": TextBlock,
-            "contacts?": [Contact],
-            "links?": [Link],
-            "socialMedia?": [SocialMediaLink],
-            "images?": [Image],
-            "activityList?": [],
-            "districtList?": [],
+            "contacts?": arrContacts,
+            "links?": arrLinks,
+            "socialMedia?": arrSocialMedias,
+            "images?": arrImages,
+            "activityList?": activityList,
+            "districtList?": districtList,
         }
         // extends Address, LatLng
-        var Association : JSON = <JSON> obj
+        let Association : JSON = <JSON> obj
 
         return { ...Association, ...Address, ...LatLng }
         // return {...LatLng}
@@ -200,11 +264,11 @@ function isNotEmpty(value) {
     return Object.keys(value).length !== 0
 }
 
-var fs = require("fs")
-var odsTable = o.calcReadTable()
-var jsonObjectFull = calcGeoData(odsTable)
+let fs = require("fs")
+let odsTable = o.calcReadTable()
+let jsonObjectFull = calcGeoData(odsTable)
 // remove empty elements
-var jsonObject = jsonObjectFull.filter(isNotEmpty)
+let jsonObject = jsonObjectFull.filter(isNotEmpty)
 
 const fout = "./out.umap"
 fs.writeFile(fout, JSON.stringify(jsonObject, null, 4), (err) => {
