a10a9901e7d1b0c13ffc01bc1d29661b07d71d67
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-06-19 15:25:00 +0200

    Fix district -> districts; cleanup

--- map/database/api/districts-options/create-district-options.php
+++ map/database/api/districts-options/create-district-options.php
@@ -6,8 +6,7 @@ require '../database.php';
 
 $postdata = file_get_contents("php://input");
 
-function endsWith($haystack, $needle)
-{
+function endsWith($haystack, $needle) {
     $length = strlen($needle);
     if (!$length) {
         return true;
@@ -19,14 +18,13 @@ if (isset($postdata) && !empty($postdata)) {
     $request = json_decode($postdata);
 
     // Update.
-    $sql = "UPDATE district SET current = 0; 
+    $sql = "UPDATE districts SET current = 0;
 
             ";
 
     $sql .= "REPLACE INTO districts (value, label, category, current) VALUES";
 
     foreach ($request as $i => $item) {
-
         $label = '';
         $value = '';
         $category = null;
@@ -36,15 +34,12 @@ if (isset($postdata) && !empty($postdata)) {
             if ($k == 'label') {
                 $label = $v;
             }
-
             if ($k == 'value') {
                 $value = $v;
             }
-
             if ($k == 'category') {
                 $category = $v;
             }
-
             if ($k == 'orderIndex') {
                 $orderIndex = $v;
             }
@@ -70,27 +65,20 @@ if (isset($postdata) && !empty($postdata)) {
 
     if (mysqli_multi_query($con, $sql)) {
         do {
-
             if ($result = mysqli_store_result($con)) {
                 while ($row = mysqli_fetch_row($result)) {
                     printf("%s\n", $row[0]);
                 }
                 mysqli_free_result($result);
             }
-
             if (mysqli_more_results($con)) {
                 printf("-----------------\n");
             }
         } while (mysqli_next_result($con));
-
         http_response_code(204);
-
     } else {
-
         echo $sql;
-
         echo mysqli_error($con) . ": " . mysqli_error($con) . "\n";
         return http_response_code(422);
-
     }
 }
