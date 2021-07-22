fd91bb3c41a78b2017e063684d7dd8abb42d2f05
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-07-22 16:25:34 +0200

    [map] markers for assocs w/o public address have different color

--- map/app-map/src/app/osm-map/osm-map.component.ts
+++ map/app-map/src/app/osm-map/osm-map.component.ts
@@ -29,6 +29,7 @@ import RenderFeature from 'ol/render/Feature';
 import Geometry from 'ol/geom/Geometry';
 import Icon from 'ol/style/Icon';
 import {createEmpty, extend, Extent} from 'ol/extent';
+import hexToRgba from 'hex-to-rgba'; // for the getActiveMarkerImgColor method
 // @ts-ignore
 import AnimatedCluster from 'ol-ext/layer/AnimatedCluster';
 import {
@@ -48,6 +49,17 @@ import {
   ]
 })
 export class OsmMapComponent implements OnInit, OnDestroy {
+
+  // TODO DRY - see app-form/src/app/services/ods-table/geo.ts
+  noPublicAddress(normAddr: string | undefined) : boolean {
+    if (normAddr) {
+        return !!normAddr.match(/.*keine|Postfach.*/i)
+    }
+    else {
+      return false
+    }
+  }
+
   sidebarExpanded = true;
   SIDEBAR_ANIMATION_DURATION = 300;
 
@@ -75,6 +87,8 @@ export class OsmMapComponent implements OnInit, OnDestroy {
   // @ts-ignore
   associations: Association[] = [];
   filteredAssociations: Association[] = [];
+
+  noPubAddrAssocIds: string[] = []
 
   @ViewChild('osmContainer', {static: true}) osmContainer!: ElementRef<HTMLDivElement>;
   @ViewChild('autoComplete', {static: true}) autoComplete?: AutoComplete;
@@ -114,6 +128,9 @@ export class OsmMapComponent implements OnInit, OnDestroy {
       });
     }
 
+    const noPubAddrAssocs: Association[] = this.associations.filter(
+      (a: Association) => this.noPublicAddress(a.addressLine1) )
+    this.noPubAddrAssocIds = noPubAddrAssocs.map((a: Association) => a.id);
     this.filteredAssociations = this.associations;
 
     if (this.osmContainer?.nativeElement?.clientWidth < 360 && this.sidebarExpanded) {
@@ -208,6 +225,7 @@ export class OsmMapComponent implements OnInit, OnDestroy {
     const isFiltered: boolean = featureIds.some((id: string) => filteredIds.includes(id));
     const size = originalFeatures?.length;
     let style;
+    const baseColor = '#d13858' // '#ed2227'
     if (!style) {
       if (size && size > 1) {
         style = new Style({
@@ -217,7 +235,7 @@ export class OsmMapComponent implements OnInit, OnDestroy {
               color: '#231f20',
             }),
             fill: new Fill({
-              color: allIncluded ? '#ed2227' : isFiltered ? '#B47172' : '#989898',
+              color: allIncluded ? baseColor : isFiltered ? '#B47172' : '#989898',
             }),
           }),
           text: new Text({
@@ -229,9 +247,17 @@ export class OsmMapComponent implements OnInit, OnDestroy {
           }),
         });
       } else {
+        const noPubAddr: boolean = featureIds.some(
+          (id: string) => this.noPubAddrAssocIds.includes(id));
+        const noPubAddrColor = '#00bfff' // DeepSkyBlue
+        const color = noPubAddr? noPubAddrColor : baseColor
+
         style = new Style({
           image: new Icon({
-            img: isFiltered ? this.getActiveMarkerImg() : this.getInactiveMarkerImg(),
+            img: isFiltered ?
+              // this.getActiveMarkerImgColor(color)
+              this.getActiveMarkerImg(noPubAddr)
+              : this.getInactiveMarkerImg(noPubAddr),
             imgSize: [48, 48],
             anchor: [0.5, 1]
           })
@@ -508,21 +534,43 @@ export class OsmMapComponent implements OnInit, OnDestroy {
     this.cluster.setSource(this.clusterSource);
   }
 
+  // /**
+  //  * return the active marker image element as an svg-path element. The colors
+  //  * cannot be in the hex format. Either color-name or RGBA formats are allowed, WTF?
+  //  */
+  // getActiveMarkerImgColor(hexFillColor : string): HTMLImageElement {
+  //   const markerImg: HTMLImageElement = this.renderer2.createElement('img');
+  //   const fill = hexToRgba(hexFillColor)
+  //   const svg =
+  //     '<svg xmlns="http://www.w3.org/2000/svg">'+
+  //     '<path fill="'+fill+'" stroke="white" stroke-width="0.50744" d="M 34.58637,33.56877 C 36.5425,30.96358 37.74628,27.7965 37.74628,24.27184 37.74628,15.99655 31.1255,9.25372 23,9.25372 c -8.1255,0 -14.74628,6.74283 -14.74628,15.01812 0,3.52466 1.25394,6.69174 3.15992,9.29693 C 14.37293,37.50208 23,48.58688 23,48.58688 c 0,0 8.62708,-11.0848 11.58637,-15.01811 z" id="path212" />'+
+  //     '<circle style="fill:white;fill-rule:evenodd" id="path116" cx="23" cy="23.642452" r="5.6927967" />'+
+  //     '</svg>';
+  //   // console.log(attr)
+
+  //   const attr = 'data:image/svg+xml;utf-8,'+svg;
+  //   markerImg.setAttribute('src', attr);
+  //   return markerImg;
+  // }
+
   /**
-   * return the active marker image element (red marker)
+   * return the active marker image element.
    */
-  getActiveMarkerImg(): HTMLImageElement {
+  getActiveMarkerImg(noPubAddr: boolean): HTMLImageElement {
     const markerImg: HTMLImageElement = this.renderer2.createElement('img');
-    markerImg.setAttribute('src', 'assets/marker-small.png');
+    const attr = noPubAddr ? 'assets/pin-DeepSkyBlue.png' : 'assets/pin-prod.png'
+    markerImg.setAttribute('src', attr);
     return markerImg;
   }
 
   /**
    * return the inactive marker image element (gray marker)
    */
-  getInactiveMarkerImg(): HTMLImageElement {
+  getInactiveMarkerImg(noPubAddr: boolean): HTMLImageElement {
     const markerImg: HTMLImageElement = this.renderer2.createElement('img');
-    markerImg.setAttribute('src', 'assets/marker-inactive-small.png');
+    // TODO different colors for inactive associations with no public address?
+    const attr = noPubAddr ? 'assets/pin-inactive-small.png' : 'assets/pin-inactive-small.png'
+    markerImg.setAttribute('src', attr);
     return markerImg;
   }
 
