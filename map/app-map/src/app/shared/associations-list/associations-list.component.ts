bac1ce6709bfcaf11ba9ea535e30486b23d7b8b7
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-11-16 19:57:11 +0100

    css: map-marker icons in the list

--- map/app-map/src/app/shared/associations-list/associations-list.component.ts
+++ map/app-map/src/app/shared/associations-list/associations-list.component.ts
@@ -11,7 +11,10 @@ export class AssociationsListComponent implements OnInit {
   @Input() associations: Association[] = [];
   @Input() identifiedByFieldName = 'name';
   @Input() selectedAssociationField: any = undefined;
-  @Input() primeNgIconClass = 'pi pi-comment';
+  @Input() primeNgPubAddr           = 'pi pi-map-marker pubAddr';
+  @Input() primeNgPubAddrSelected   = 'pi pi-map pubAddr';
+  @Input() primeNgNoPubAddr         = 'pi pi-map-marker noPubAddr';
+  @Input() primeNgNoPubAddrSelected = 'pi pi-map noPubAddr';
 
   @Output() selected: EventEmitter<Association> = new EventEmitter<Association>();
 
