1c694e38c6d611fa7d72929337a23db3fd3e06ee
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2021-11-27 14:58:46 +0100

    guix: improve db-connect-test.php

--- etc/db-connect-test.php
+++ etc/db-connect-test.php
@@ -1,26 +1,33 @@
 <?php
-# Fill our vars and run on cli
-# $ php -c /usr/etc -f db-connect-test.php
+# Fill out the db* vars below and run from the command line:
+#   php -c /usr/etc -f /etc/db-connect-test.php
 
 $php_inipath = php_ini_loaded_file();
 
+// WTF?!?
+// To be able to display line breaks when using single quotation marks, they
+// must be surrounded by double quotation marks and linked to the first string.
+
+
 if ($php_inipath) {
-    echo 'Loaded php.ini is: ' . $php_inipath . "\n";
+    echo "Loaded php.ini: " . $php_inipath . "\n";
 } else {
-    die('php.ini file is not loaded');
+    die("php.ini file is not loaded");
 }
 
-$dbname = 'associations';
-$dbuser = 'bost';
-$dbpass = '';
 $dbhost = '';
+$dbuser = $_ENV["USER"];
+$dbpass = '';
+$dbname = 'associations';
+
+echo "Connecting to dbhost: '$dbhost' dbuser: '$dbuser' dbname: '$dbname' ...\n";
 
 $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_error() . "\n";
     exit();
 }
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'\n");
+mysqli_select_db($link, $dbname) or die("Couldn't connect to '$dbname'\n");
 
 $test_query = "SHOW TABLES FROM $dbname";
 $result = mysqli_query($link, $test_query);
@@ -31,9 +38,5 @@ while($tbl = mysqli_fetch_array($result)) {
     #echo $tbl[0]."<br />\n";
 }
 
-if (!$tblCnt) {
-    echo "There are no tables<br />\n";
-} else {
-    echo "There are $tblCnt tables<br />\n";
-}
+echo "Database $dbname contains $tblCnt tables.\n";
 ?>
