6b2524caf27a81772095cf21616619a83fcad24b
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-02 17:02:01 +0200

    guix: reset.sh deletes all node_modules

--- reset.sh
+++ reset.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
-rm -rf var/lib/mysql/data/*
-rm -rf var/log/*
+set -x  # Print commands and their arguments as they are executed.
+rm -rf \
+   var/lib/mysql/data/* \
+   var/log/* \
+   ./node_modules/ \
+   ./map/app-form/node_modules/ \
+   ./map/app-map/node_modules/
+
 git checkout etc/ \
     map/app-map/src/environments/ \
     map/app-form/src/environments/
