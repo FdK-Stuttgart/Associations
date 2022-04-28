03f682720acf71d35135077b35aa822547a3ac50
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-28 13:09:40 +0200

    lein new re-frame cmap +garden +cider +kondo +git-inject

---
+++ cmap/src/cmap/views.cljs
@@ -0,0 +1,15 @@
+(ns cmap.views
+  (:require
+   [re-frame.core :as re-frame]
+   [cmap.styles :as styles]
+   [cmap.subs :as subs]
+   [cmap.config :as config]
+   ))
+
+(defn main-panel []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [:h1
+      {:class (styles/level1)}
+      "Hello from " @name". Version " config/version]
+     ]))
