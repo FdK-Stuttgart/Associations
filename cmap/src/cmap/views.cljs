39bd781bd75b6798cb1042223a90d3f985c96793
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-28 20:11:46 +0200

    OpenLayers as a React component

--- cmap/src/cmap/views.cljs
+++ cmap/src/cmap/views.cljs
@@ -4,12 +4,26 @@
    [cmap.styles :as styles]
    [cmap.subs :as subs]
    [cmap.config :as config]
+   [reagent.core :as reagent]
+   ["ol/proj" :as ol]
+   ["rlayers" :as rl]
+   #_["ol/css"] ;; TODO use ol/css instead of <link rel="stylesheet" ...>
    ))
+
+(def props
+  {:width "50%"
+   :height "90vh"
+   :initial {:center (js->clj (.fromLonLat ol #js [2.364 48.82]))
+             :zoom 11}})
+
+(def RMap (reagent/adapt-react-class rl/RMap))
+(def ROSM (reagent/adapt-react-class rl/ROSM))
+
+;; (.log js/console "RMap" RMap)
+;; (.log js/console "ROSM" ROSM)
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1
-      {:class (styles/level1)}
-      "Hello from " @name". Version " config/version]
-     ]))
+     [RMap props [ROSM]]
+     [:div {:class (styles/level1)} @name" v"config/version]]))
