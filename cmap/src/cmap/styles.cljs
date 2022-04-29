bed29963534496044704618a3a588594eda9a0d1
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-04-29 10:56:04 +0200

    Cascade Style Sheets

--- cmap/src/cmap/styles.cljs
+++ cmap/src/cmap/styles.cljs
@@ -12,8 +12,19 @@
  ([dir c1 p1 c2 p2]
   [dir [c1 p1] [c2 p2]]))
 
+#_(defglobal d [:body {:color :red}])
+
+(do
+  (defn universal-selector-factory$ [style-name24356 params24357]
+    {:name style-name24356, :css (spade.runtime/compile-css [[:* {:box-sizing :border-box}]])})
+  (let [factory-name24358 (spade.util/factory->name universal-selector-factory$)]
+    (def universal-selector
+      (spade.runtime/ensure-style! :global factory-name24358 universal-selector-factory$ nil))))
+
 (defglobal defaults
-  [:body
+  [
+   ;; :&:* {:box-sizing :border-box}
+   :body
    {:color               :red
     :background-color    :#ddd
     :background-image    [(linear-gradient :white (px 2) :transparent (px 2))
@@ -26,3 +37,29 @@
 (defclass level1
   []
   {:color :green})
+
+;; Create two unequal columns that floats next to each other
+(defclass column []
+  {
+   ;; :flex "70%"
+   :float "left"
+   :padding (px 10)
+   :height (px 300)
+   })
+
+(defclass left [] {:width "75%"})
+(defclass right [] {:width "25%"})
+
+;; Clear floats after the columns
+;; (defclass row [] {:content "\"\"" :display "table" :clear "both"})
+(do
+  (defn row-factory$ [style-name23148 params23149]
+    (let [style23151 [(str "." style-name23148 "::after")
+                      {:content "\"\"", :display "table", :clear "both"}]]
+      {:css (spade.runtime/compile-css style23151), :name style-name23148}))
+  (let [factory-name23150 (spade.util/factory->name row-factory$)]
+    (defn row []
+      (spade.runtime/ensure-style! :class factory-name23150 row-factory$ []))))
+
+
+
--- cmap/src/cmap/views.cljs
+++ cmap/src/cmap/views.cljs
@@ -5,25 +5,56 @@
    [cmap.subs :as subs]
    [cmap.config :as config]
    [reagent.core :as reagent]
-   ["ol/proj" :as ol]
+
+   ["ol/proj" :as proj]
+   ["ol/geom" :as geom]
+
    ["rlayers" :as rl]
-   #_["ol/css"] ;; TODO use ol/css instead of <link rel="stylesheet" ...>
+   ["rlayers/style" :as style]
+   #_["ol/css" :as css] ;; TODO use ol/css instead of <link rel="stylesheet" ...>
    ))
 
 (def props
-  {:width "50%"
+  {
+   ;; :width "50%"
    :height "90vh"
-   :initial {:center (js->clj (.fromLonLat ol #js [2.364 48.82]))
+   :initial {:center (js->clj (.fromLonLat proj #js [2.364 48.82]))
              :zoom 11}})
 
+;; import { RMap, ROSM, RLayerVector, RFeature, RPopup } from "rlayers";
 (def RMap (reagent/adapt-react-class rl/RMap))
 (def ROSM (reagent/adapt-react-class rl/ROSM))
+(def RLayerVector (reagent/adapt-react-class rl/RLayerVector))
+(def RFeature (reagent/adapt-react-class rl/RFeature))
+(def RPopup (reagent/adapt-react-class rl/RPopup))
+
+;; import { RStyle, RIcon, RFill, RStroke } from "rlayers/style";
+(def RStyle (reagent/adapt-react-class style/RStyle))
+(def RIcon (reagent/adapt-react-class style/RIcon))
 
 ;; (.log js/console "RMap" RMap)
 ;; (.log js/console "ROSM" ROSM)
 
+(def coords {:origin        #js [2.364 48.82]
+             :ArcDeTriomphe #js [2.295 48.8737]
+             :PlaceDItalie  #js [2.355 48.831]
+             :Bastille      #js [2.369 48.853]
+             :TourEiffel    #js [2.294 48.858]
+             :Montmartre    #js [2.342 48.887]
+             })
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [RMap props [ROSM]]
-     [:div {:class (styles/level1)} @name" v"config/version]]))
+     [:div {:class (styles/row)}
+      [:div {:class [(styles/column) (styles/left)]}
+       ;; "left"
+       [RMap props [ROSM]
+        [RLayerVector {:zIndex 10}
+         [RFeature {:geometry (new geom/Point (.fromLonLat proj (:ArcDeTriomphe coords)))}
+          [RStyle
+           [RIcon {:src "/img/location.svg" :anchor [0.5 0.8]}]]]]]]
+      [:div {:class [(styles/column) (styles/right)]} "right"]
+      ]
+     [:div {:class (styles/level1)} @name" v"config/version]]
+    ))
