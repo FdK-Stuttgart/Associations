5f92ebdcc4ab9c2908428da28757e3f5d4e594b2
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2022-11-17 14:10:25 +0100

    frontend: Add the map component

---
+++ src/cljs/fdk/cmap/react_components.cljs
@@ -0,0 +1,15 @@
+(ns fdk.cmap.react-components
+  "Third party react components"
+  (:require
+   [reagent.core :as reagent]
+   ["semantic-ui-react" :as rsu]
+   #_["react-leaflet" :as rle]
+   ))
+
+(def Tab (reagent/adapt-react-class rsu/Tab))
+
+;; (def MapContainer (reagent/adapt-react-class rle/MapContainer))
+;; (def TileLayer (reagent/adapt-react-class rle/TileLayer))
+;; (def Marker (reagent/adapt-react-class rle/Marker))
+;; (def Popup (reagent/adapt-react-class rle/Popup))
+;; (def useMap (reagent/adapt-react-class rle/useMap))
