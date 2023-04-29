74865f8c4cd529e9e257a63b375b509dcf61d0f9
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-04-29 13:35:30 +0200

    manifest: add node@18.16.0

--- run.sh
+++ run.sh
@@ -63,6 +63,9 @@ fi
 # Warning: hardcoded path! See also .bashrc
 # --share=$HOME/dec/fdk=$HOME/dec/fdk \
 
+# set up my local time
+# --share=/etc/localtime=/etc/localtime
+
 set -x
 guix shell \
      --root=./persistent-profile \
@@ -70,7 +73,7 @@ guix shell \
      --container --network \
      --preserve=^fdk \
      --share=/usr/bin \
-     --share=$HOME/dev/node-v14.20.0=$HOME/dev/node-v14.20.0 \
+     --share=/etc/localtime=/etc/localtime \
      --share=$HOME/.bash_history=$HOME/.bash_history \
      --share=$HOME/.config/fish=$HOME/.config/fish \
      --share=$HOME/.gitconfig=$HOME/.gitconfig \
