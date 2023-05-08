1f13e8c70760e85423e199cd530a22c9df49cea3
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-05-08 15:15:29 +0200

    popup-content-with-marks

--- src/cljs/fdk/cmap/popup.cljs
+++ src/cljs/fdk/cmap/popup.cljs
@@ -27,9 +27,36 @@
 
 (enable-console-print!)
 
+(def filter-keys
+  [:name
+   ;; :shortName
+
+   ;; :addr
+   :street
+   :postcode-city
+   :city
+   :country
+
+   :goals
+   :activities
+
+   ;; :contacts
+   :pobox :phone :fax
+
+   :email
+
+   ;; :imageurl    ;; :url :alttext
+   ;; :links       ;; :url :linkText
+   ;; :socialmedia ;; :url :linkText :platform
+
+   ;; :districts   ;; TODO
+   ])
+
 (def pattern-atom (reagent/atom nil))
 
 (defn mark-string [s]
+  #_(js/console.log "[mark-string]" "s" s)
+  #_(js/console.log "[mark-string]" "@pattern-atom" @pattern-atom)
   (if-let [pattern-orig @pattern-atom]
     (let [pattern (s/lower-case pattern-orig)]
       ((comp
@@ -44,16 +71,15 @@
        pattern))
     s))
 
-(defn popup-content
+(defn- popup-content
   "addr has only 'keine öffentliche Anschrift'
-  (popup-content prm)
+  (popup-content marker-data filter-data)
   "
   [{:keys [k name addr street postcode-city districts email activities goals
            imageurl links socialmedia
-           pobox phone fax
-           ] :as prm}]
-  #_(js/console.log "(keys prm)" (count (keys prm)) (keys prm))
-  #_(js/console.log "phone" phone)
+           pobox phone fax] :as marker-data}
+   filter-data]
+  #_(js/console.log "(keys marker-data)" (count (keys marker-data)) (keys marker-data))
   [:div
    {:class
          "association-container osm-association-container"
@@ -62,20 +88,13 @@
     }
    [:div {:class "osm-association-inner-container"}
     [:div {:class "association-title"}
-     (into [:h2]
-           ;; (mark-string name) ;; doesn't work
-
-           ;; [mark-string name] ;; Produces:
-           ;; Warning: Functions are not valid as a React child. This may happen
-           ;; if you return a Component instead of <Component /
-
-           (:f> mark-string name))]
+     [:h2 (:name filter-data)]]
     [:div {:class "association-images"}
      [:div {:class "association-image"}
       [:img {:src imageurl :alt ""}]]]
     [:div {:class "association-address"}
-     [:p {:class "street"} street]
-     [:p {:class "postcode-city"} postcode-city]
+     [:p {:class "street"} (:street filter-data)]
+     [:p {:class "postcode-city"} (:postcode-city filter-data)]
      [:p {:class "name"} [:strong addr]]]
 
     (when (or (seq phone) (seq fax) (seq email))
@@ -86,23 +105,24 @@
            [:div {:class "social-media-icon mini-icon"}
             [:img {:src "assets/phone.png" :alt ""}]]
            [:p {:class "phone"}
-            [:a {:href (str "tel:" phone)} phone]]])
+            [:a {:href (str "tel:" phone)} (:phone filter-data)]]])
         (when (seq fax) ;; i.e. (not (empty? ...))
           [:div {:class "association-contact-row"}
            [:div {:class "social-media-icon mini-icon"}
             [:img {:src "assets/fax.png" :alt ""}]]
            [:p {:class "fax"}
-            [:a {:href (str "fax:" fax)} fax]]])
+            [:a {:href (str "fax:" fax)} (:fax filter-data)]]])
         (when (seq email) ;; i.e. (not (empty? ...))
           [:div {:class "association-contact-row"}
            [:div {:class "social-media-icon mini-icon"}
             [:img {:src "assets/mail.png" :alt ""}]]
            [:p {:class "mail"}
-            [:a {:href (str "mailto:" email)} email]]])]])
+            [:a {:href (str "mailto:" email)} (:email filter-data)]]])]])
     [:div {:class "association-description"}
-     [:h3 (de :fdk.cmap.lang/goals)] goals]
+     [:h3 (de :fdk.cmap.lang/goals)]
+     (:goals filter-data)]
     [:div {:class "association-description"}
-     [:h3 (de :fdk.cmap.lang/activities)] activities]
+     [:h3 (de :fdk.cmap.lang/activities)] (:activities filter-data)]
     [:div {:class "association-active-in"}
      [:h3 (de :fdk.cmap.lang/activity-areas)]
      [:div {:class "association-chips-container"}
@@ -140,3 +160,22 @@
          (partial apply mapv vector) ;; transpose
          (juxt :platforms :urls))
         socialmedia)])]])
+
+(defn popup-content-with-marks
+  "addr has only 'keine öffentliche Anschrift'
+  (popup-content-with-marks marker-data)
+  "
+  [marker-data]
+  (let [m (select-keys marker-data filter-keys)]
+    ((comp
+      (partial popup-content marker-data)
+      (partial into {})
+      (partial map (fn [k]
+                     ((comp
+                       (partial hash-map k)
+                       (partial into [:span])
+                       mark-string
+                       (partial get-in m)
+                       vector)
+                      k))))
+     filter-keys)))
